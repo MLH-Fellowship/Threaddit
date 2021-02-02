c8c49c13c10aeada0142cca85871e32bf3eceef4
Diana Mukhanova <diananova2508@gmail.com>
2021-02-02 16:41:28 +0100

    added Bootstrap

--- app/src/components/Navigation.js
+++ app/src/components/Navigation.js
@@ -2,6 +2,9 @@ import React from "react";
 import Login from './Login';
 import Signup from './Signup';
 import Home from './Home';
+import { Navbar, Nav, Form, FormControl, Button, NavItem } from 'react-bootstrap';
+
+
 import {
   BrowserRouter as Router,
   Switch,
@@ -13,20 +16,15 @@ export default function Navigation() {
     return (
         <Router>
           <div>
-            <nav>
-              <ul>
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
-                <li>
-                  <Link to="/login">Login</Link>
-                </li>
-                <li>
-                  <Link to="/signup">Sign up</Link>
-                </li>
-              </ul>
-            </nav>
-    
+            <Navbar bg="dark" variant="dark">
+            <Nav className="mr-auto">
+                  <Nav.Link as={Link} to="/" >Home</Nav.Link>
+          
+                  <Nav.Link as={Link} to="/login" >Login</Nav.Link>
+           
+                  <Nav.Link as={Link} to="/signup" >Sign up</Nav.Link>
+              </Nav>
+            </Navbar>
             <Switch>
               <Route path="/signup">
                 <Signup />
--- app/src/index.js
+++ app/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/Header';
 import Navigation from './components/Navigation';
-import MyRouter from './components/Router';
 
 ReactDOM.render(
   <div>
