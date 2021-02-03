5f0592b8ca38ce29129df01ce467799da25e9fcd
Gita Alekhya Paul <54375111+gitaalekhyapaul@users.noreply.github.com>
2021-02-03 12:22:07 +0530

    style(client): refactor router code

--- app/src/index.js
+++ app/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import Navigation from "./components/Shared/Navigation";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Amplify from "aws-amplify";
@@ -8,8 +9,8 @@ import awsconfig from "./aws-exports";
 Amplify.configure(awsconfig);
 
 ReactDOM.render(
-  <div>
+  <Router>
     <Navigation />
-  </div>,
+  </Router>,
   document.getElementById("root")
 );
