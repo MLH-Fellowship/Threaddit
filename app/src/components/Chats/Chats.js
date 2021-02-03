ce11b670209324578b821dd2ba21b366db8f3204
Diana Mukhanova <diananova2508@gmail.com>
2021-02-03 23:49:27 +0100

    added basic form for creating posts

--- app/src/components/Chats/Chats.js
+++ app/src/components/Chats/Chats.js
@@ -1,6 +1,8 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { withAuthenticator } from "@aws-amplify/ui-react";
 import { AuthContext } from "../../store/AuthContext";
+import Richtext from "../Slate/Richtext";
+
 const Chats = () => {
   const authContext = useContext(AuthContext);
 
@@ -8,7 +10,25 @@ const Chats = () => {
     authContext.checkAuth();
   }, [authContext]);
 
-  return <h2>Chat Section</h2>;
+  const [title, setTitle] = useState("");
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    let content = localStorage.getItem('content');
+    //TODO: save to database
+    console.log(content);
+    console.log(title);
+}
+
+  return <div>
+    <form  onSubmit={handleSubmit}>
+    <label>
+    Title
+    <input type="text" value={title} onChange={e => setTitle(e.target.value)} />
+  </label>
+    <Richtext/>
+    <input type="submit" value="Submit" />
+    </form>
+  </div>;
 };
 
 export default withAuthenticator(Chats, true);
