693e344ded2a314b6c1a6e5b67c2b5993c68542b
Gita Alekhya Paul <54375111+gitaalekhyapaul@users.noreply.github.com>
2021-02-03 15:56:07 +0530

    style(client/navbar): updated UI

--- app/src/components/Shared/Navigation.js
+++ app/src/components/Shared/Navigation.js
@@ -5,6 +5,7 @@ import { Navbar, Nav } from "react-bootstrap";
 import { AuthContext } from "../../store/AuthContext";
 import { Switch, Route, Link } from "react-router-dom";
 import { AmplifySignOut } from "@aws-amplify/ui-react";
+import { ChatRightTextFill } from "react-bootstrap-icons";
 
 const Navigation = () => {
   const authContext = useContext(AuthContext);
@@ -17,19 +18,27 @@ const Navigation = () => {
 
   return (
     <>
-      <Navbar bg="light">
+      <Navbar bg="dark" variant="dark" className="justify-content-end">
         <Navbar.Brand as={Link} to="/">
+          <ChatRightTextFill color="white" size="1.5rem" />
+          {"  "}
           Threaddit
         </Navbar.Brand>
         <Nav className="mr-auto">
-          <Nav.Link as={Link} to="/">
-            Home
-          </Nav.Link>
-          <Nav.Link as={Link} to="/chats">
-            Chats
-          </Nav.Link>
+          <Nav.Item>
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link as={Link} to="/chats">
+              {authContext.isAuth ? "Chats" : "Login"}
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+        <Nav className="ml-auto">
           {authContext.isAuth && (
-            <Nav.Item>
+            <Nav.Item className="justify-content-end">
               <AmplifySignOut onClick={() => handleLogout()} />
             </Nav.Item>
           )}
