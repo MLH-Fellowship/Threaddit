962aed0856a3e0adf8cb4648214210a892ba62c4
Gita Alekhya Paul <54375111+gitaalekhyapaul@users.noreply.github.com>
2021-02-05 03:32:51 +0530

    fix(client): fix posts not rendering

--- app/src/components/Home/Home.tsx
+++ app/src/components/Home/Home.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useMemo, useState } from "react";
-import {listQuery} from "../Chats/queries";
+import { listQuery } from "../Chats/queries";
 import { Editable, withReact, useSlate, Slate } from "slate-react";
 import {
   Editor,
@@ -10,43 +10,56 @@ import {
 } from "slate";
 import { withHistory } from "slate-history";
 
+interface PostSchema {
+  title: string;
+}
 
 const Home = () => {
-  const editor = useMemo(() => withReact(createEditor()), [])
+  const editor = useMemo(() => withReact(createEditor()), []);
   const [data, setData] = useState("");
-  let posts:any[] = [];
-  let listPosts; 
+  const [posts, setPosts] = useState<Array<PostSchema>>([]);
   useEffect(() => {
     async function getPosts() {
-      const data = await listQuery()
-      setData(data)
-      console.log(data)
-      posts = JSON.parse(data).data.listPosts.items
-      console.log("title: "+posts[0].title)
-      listPosts = posts.map((post:any) => <li key={post.title.toString()}>{post.title}</li>)
-      console.log("posts"+JSON.stringify(listPosts))
-  }
-  getPosts();
-  }, [])
+      const data = await listQuery();
+      setData(data);
+      console.log(data);
+      setPosts(JSON.parse(data).data.listPosts.items);
+    }
+    getPosts();
+  }, [posts]);
 
-// 
-  return( <div>
-  <h2>Welcome to Threaddit!</h2>
-  <ul>{listPosts}</ul>
-  <div style={{ userSelect: "none" }} contentEditable={false}>
-   
-  <ul>{posts.map((post:any) => <li key={post.title.toString()}>{post.title}</li>)}</ul>
-  <Slate style={{ userSelect: "none" }} contentEditable={false}
-      editor={editor}
-      value={JSON.parse('[{\"type\":\"paragraph\",\"children\":[{\"text\":\"cooontent\"}]}]')}
-      onChange={value => {
-        console.log(value)
-      }}
-    >
-      <Editable style={{ userSelect: "none" }} contentEditable={false}/>
-    </Slate>
+  //
+  return (
+    <div>
+      <h2>Welcome to Threaddit!</h2>
+      <ul>
+        {posts &&
+          posts.map((post: any) => (
+            <li key={post.title.toString()}>{post.title}</li>
+          ))}
+      </ul>
+      <div style={{ userSelect: "none" }} contentEditable={false}>
+        <ul>
+          {posts.map((post: any) => (
+            <li key={post.title.toString()}>{post.title}</li>
+          ))}
+        </ul>
+        <Slate
+          style={{ userSelect: "none" }}
+          contentEditable={false}
+          editor={editor}
+          value={JSON.parse(
+            '[{"type":"paragraph","children":[{"text":"cooontent"}]}]'
+          )}
+          onChange={(value) => {
+            console.log(value);
+          }}
+        >
+          <Editable style={{ userSelect: "none" }} contentEditable={false} />
+        </Slate>
+      </div>
     </div>
-  </div>);
+  );
 };
 
 export default Home;
